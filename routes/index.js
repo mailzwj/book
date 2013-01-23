d240a4ab598866a4afc082528555c142efa2aa21
Mr.Zheng <mailzwj@126.com>
2013-01-23 14:09:09 +0800

    添加mac开发环境

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,36 @@
  * GET home page.
  */
 
+var config = require("../config");
+var db = config.db;
+var crypto = require("crypto");
+var errs = config.errs;
+var coll = db.collection("users");
 exports.index = function(req, res){
-	res.render("index", {title: "我的图书管理系统"});
+	var err = req.param("err");
+	var data = {title: "我的图书管理系统"};
+	if(err){
+		data.err = errs[err];
+	}
+	var un = req.param("username");
+	var up = req.param("userpass");
+	if(up){
+		var ch = crypto.createHash("sha1");
+		ch.update(up);
+		up = ch.digest("hex");
+		coll.findOne({"$or": [{nick: un}, {realname: un}, {email: un}], password: up}, function(err, rs){
+			if(err){
+				console.log("Err:" + err);
+			}else{
+				if(rs){
+					data.user = rs;
+					res.render("index", data);
+				}else{
+					res.redirect("/?err=10002");
+				}
+			}
+		});
+	}else{
+		res.render("index", data);
+	}
 };
--- server.js
+++ server.js
@@ -3,13 +3,14 @@
  * Module dependencies.
  */
 var express = require('express')
+    , cfg = require("./config")
     , routes = require('./routes')
     //, detail = require('./routes/detail')
     , http = require('http')
     , path = require('path');
 var app = express();
 app.configure(function(){
-    app.set('port', process.env.PORT || 80);
+    app.set('port', process.env.PORT || cfg.ports.mac);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.favicon());
@@ -24,7 +25,7 @@ app.configure(function(){
 app.configure('development', function(){
     app.use(express.errorHandler());
 });
-app.get('/', routes.index);
+app.all('/', routes.index);
 http.createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port'));
 });
