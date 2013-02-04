da797f24e09c322619f06ccb59547571129f6b79
wb-zhengwujiang.z <wb-zhengwujiang.z@taobao.com>
2013-02-04 11:47:06 +0800

    新增用户模块(user.js),图书模块(book.js)

---
+++ routes/book.js
@@ -0,0 +1,43 @@
+﻿/*
+ * about books.
+ */
+
+var config = require("../config");
+var db = config.db;
+var crypto = require("crypto");
+var coll = db.collection("users");
+var bcol = db.collection("books");
+exports.add = function(bookname, pic, author, publish_house, publish_date, recommend, book_cate){
+	//add book
+};
+
+//修改书籍信息
+exports.update = function(bookname, pic, author, publish_house, publish_date, recommend, book_cate){
+	//update book
+};
+
+//根据bookid删除书籍
+exports.del = function(bid){
+	//delete book
+};
+
+//判断图书可借状态，返回bool
+exports.canborrow = function(uid){
+	//是否可借
+};
+
+exports.pushborrow = function(username, bookname, book_id, book_cate, apply_time){
+	//将申请放入借书申请列表
+};
+
+exports.pullborrow = function(username, book_id){
+	//通过或拒绝的借书申请，从临时表删除记录
+};
+
+exports.pushalso = function(username, bookname, book_id, book_cate, also_time){
+	//将还书申请放入临时表
+};
+
+exports.pullalso = function(username, book_id){
+	//通过或拒绝的还书申请，从临时表删除记录
+};
---
+++ routes/user.js
@@ -0,0 +1,31 @@
+﻿/*
+ * about users.
+ */
+
+var config = require("../config");
+var db = config.db;
+var crypto = require("crypto");
+var coll = db.collection("users");
+exports.add = function(username, pwd, email){
+	//add user
+};
+
+//只允许修改密码和邮箱
+exports.update = function(pwd, email){
+	//update user
+};
+
+//根据userid删除用户
+exports.del = function(uid){
+	//delete user
+};
+
+//判断用户是否已登录，并返回bool值
+exports.islogin = function(uid){
+	//check login
+};
+
+//判断登录用户是否管理员，并返回bool值
+exports.isadmin = function(uid){
+	//check level
+};
